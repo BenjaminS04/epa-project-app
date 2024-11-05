f41b01b4232639e0af0267c581615b63b1768433
Benjamin-Scanlon_JDPLC <Benjamin.Scanlon@jdplc.com>
2024-11-05 14:35:59 +0000

    more console logs added to determine where issues are and added missing ;

--- js/app.js
+++ js/app.js
@@ -7,14 +7,17 @@ const dataPoints = {
 }
 // initialise AWS SDK
 function initAWS(region) {
+    console.log("initAWS running");
     AWS.config.region = region
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
         IdentityPoolId:'' //leave empty as app should use the instance's assigned role
     })
+    console.log("credentials set and cloudwatch to be returned");
     return new AWS.CloudWatch();
 }
 // Defines the parameters for the GetMetricData API request, async used to allow use of promise
 async function getMetrics(cloudwatch, instanceId) {
+    console.log("defining parameters for getmetricdata api");
     StartTime = new Date(new Date() - 300000); // last 5 mins
     EndTime = new Date();
     const params = {
@@ -61,36 +64,36 @@ function updateMetricsDisplay(data) {
 async function startMonitoring() {
     const instanceId = document.getElementById("instanceId").value;
     const region = document.getElementById("region").value;
-    console.log("instance ID:", instanceId)
-    console.log("region:", region)
+    console.log("instance ID:", instanceId);
+    console.log("region:", region);
 
     if (!instanceId || !region){
-        alert("please enter Instance ID and region")
+        alert("please enter Instance ID and region");
         return
     }
 
-    console.log("starting monitoring")
+    console.log("starting monitoring");
 
     monitoring = true;
     document.getElementById("stopBtn").style.display = "inline";
 
-    const cloudwatch = initAWS(region)
+    const cloudwatch = initAWS(region);
 
     async function fetchData() {
         try{
             const data = await getMetrics(cloudwatch, instanceId);
             updateMetricsDisplay(data);
         }catch (error) {
-            console.error("Error fetching metrics:", error)
+            console.error("Error fetching metrics:", error);
             stopMonitoring();
-            alert("error fetching metric data!")
+            alert("error fetching metric data!");
         }
         
         
     }
-    console.log("running wait fetch")
+    console.log("running wait fetch");
     await fetchData();
-    console.log("data fetch attempted")
+    console.log("data fetch attempted");
 
     monitoringInterval =setInterval(fetchData, 60000);
 
@@ -98,6 +101,6 @@ async function startMonitoring() {
     function stopMonitoring() {
         monitoring = false;
         clearInterval(monitoringInterval);
-        document.getElementById("stopBtn")
+        document.getElementById("stopBtn");
     }
 }
