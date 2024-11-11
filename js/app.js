48e95522b69fc74adebae3630571081a6c001575
Benjamin-Scanlon_JDPLC <Benjamin.Scanlon@jdplc.com>
2024-11-11 11:21:49 +0000

    client side js updated

--- js/app.js
+++ js/app.js
@@ -1,60 +1,11 @@
 // global variables
 let monitoring = false;
 let monitoringInterval;
-const dataPoints = {
-    cpu:[],
-    diskRead:[]
-}
-
-// Defines the parameters for the GetMetricData API request, async used to allow use of promise
-async function getMetrics(cloudwatch, instanceId) {
-    console.log("defining parameters for getmetricdata api");
-    StartTime = new Date(new Date() - 300000); // last 5 mins
-    EndTime = new Date();
-    const params = {
-        MetricDataQueries: [
-            {
-                Id: 'cpuUtilization',
-                MetricStat: {
-                    Metric: {
-                        Namespace: 'AWS/EC2',
-                        MetricName: 'CPUUtilization',
-                        Dimensions: [
-                            {
-                                Name: 'InstanceId',
-                                Value: instanceId
-                            }
-                        ]
-                    },
-                    Period: 60, // Get data at 1-minute intervals
-                    Stat: 'Average'
-                },
-                ReturnData: true
-            }
-        ],
-        StartTime: StartTime,
-        EndTime:EndTime
-    };
-
-    // returns promise to fetch metrics from cloudwatch
-    return new Promise((resolve, reject)=>{
-        cloudwatch.getMetricData(params, (err, data) =>{
-            if (err) reject(err);
-            else resolve(data);
-        });
-    });
-};
-
-//function to update metrics
-function updateMetricsDisplay(data) {
-    const timestamp = new Date().toLocaleTimeString();
-
-    document.getElementById("cpu").textContent = '${data.MetricDataResults[0].Values[0]?.toFixed(2) || 0}%';
-}
 
 async function startMonitoring() {
     const instanceId = document.getElementById("instanceId").value;
     const region = document.getElementById("region").value;
+
     console.log("instance ID:", instanceId);
     console.log("region:", region);
 
@@ -72,16 +23,26 @@ async function startMonitoring() {
 
     async function fetchData() {
         try{
-            const data = await getMetrics(cloudwatch, instanceId);
-            updateMetricsDisplay(data);
+            const response = await fetch('/api/getMetrics',{
+                method: 'POST', // use post method
+                headers: {
+                    'Content-Type':'application/json', // request body is json
+                },
+                body: JSON.stringify({instanceId, region}), // send instance id and region in request body
+            });
+
+            if (!responsse.ok) {
+                throw new Error('Network response not ok.');
+            }
+
+            const data = await response.json();
+            updateMetricsDisplay(data)
         }catch (error) {
             console.error("Error fetching metrics:", error);
             stopMonitoring();
             alert("error fetching metric data!");
         }
-        
-        
-    }
+     }
     console.log("running wait fetch");
     await fetchData();
     console.log("data fetch attempted");
@@ -92,6 +53,19 @@ async function startMonitoring() {
     function stopMonitoring() {
         monitoring = false;
         clearInterval(monitoringInterval);
-        document.getElementById("stopBtn");
+        document.getElementById("stopBtn").style.display ="none";
     }
 }
+
+//function to update metrics
+function updateMetricsDisplay(data) {
+    const cpuUtilization= data.MetricDataResults.find(
+        (metric) => metric.Id ==='cpuUtilization'
+    );
+
+    const cpuValue = cpuUtilization.Values.length > 0
+        ? cpuUtilization.Values[0].toFixed(2)
+        : '0.00';
+    document.getElementById("cpu").textContent = cpuValue +"%";
+}
+
